03896411a9543a6b7a29790ab608f42ad54f2924
nishu kumar <kumarnishu437@gmail.com>
2023-10-27 16:34:58 +0530

    updated

--- client/src/pages/crm/CrmHelpPage.tsx
+++ client/src/pages/crm/CrmHelpPage.tsx
@@ -1,11 +1,13 @@
 import { Grid } from '@mui/material'
 import createLeadUrl from "../../assets/crm/new lead.mp4"
+import createLeadPosterUrl from "../../assets/crm/1.jpg"
+
 function CrmHelpPage() {
   return (
     <>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} paddingX={1}>
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <video width="320" height="240" controls>
+          <video width="320" height="240" controls poster={createLeadPosterUrl}>
             <source src={createLeadUrl} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
