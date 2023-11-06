21514a8b89ffec63333c4d40ca70d189f5d3d71d
nishu kumar <kumarnishu437@gmail.com>
2023-11-06 12:49:22 +0530

    updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,3 +16,20 @@ function App() {
 
 
 export default App
+
+// import { FixedSizeList as List } from 'react-window';
+
+// const Row = ({ index, style }: { index: number, style: React.CSSProperties }) => (
+//   <div style={style}>Row {index}</div>
+// );
+
+// const Example = () => (
+//   <List
+//     height={150}
+//     itemCount={1000}
+//     itemSize={35}
+//     width={300}
+//   >
+//     {Row}
+//   </List>
+// );
