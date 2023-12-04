1ca839bff1b7e545cbdc955ba2e7b4f93dbf1311
unknown <kumarnishu437@gmail.com>
2023-12-04 17:14:15 +0530

    updated

--- client/src/pages/bot/ChatsPage.tsx
+++ client/src/pages/bot/ChatsPage.tsx
@@ -8,14 +8,20 @@ import ChatsTable from '../../components/tables/ChatTable'
 import { IChat } from '../../types/chat.types'
 import { LinearProgress, Stack, TextField, Typography } from '@mui/material'
 import FuzzySearch from "fuzzy-search";
+import { IUser } from '../../types/user.types'
+import { GetUsers } from '../../services/UserServices'
 
 function ChatsPage() {
     const { user } = useContext(UserContext)
     const [filter, setFilter] = useState<string | undefined>()
     const [chats, setChats] = useState<IChat[]>([])
     const [prefilterChats, setPreFilteredChats] = useState<IChat[]>([])
+    const [clientId, setClientId] = useState<string | undefined>(user?.client_id)
+    const [users, setUsers] = useState<IUser[]>([])
 
-    const { data, isSuccess, isLoading, error } = useQuery<AxiosResponse<IChat[]>, BackendError>("chats", async () => GetChats({ client_id: user?.client_id }))
+    const { data, isSuccess, isLoading, error, refetch } = useQuery<AxiosResponse<IChat[]>, BackendError>("chats", async () => GetChats({ client_id: clientId }))
+
+    const { data: usersData, isSuccess: isUsersSuccess } = useQuery<AxiosResponse<IUser[]>, BackendError>("users", GetUsers)
     useEffect(() => {
         if (filter) {
             if (chats) {
@@ -33,6 +39,12 @@ function ChatsPage() {
     }, [filter])
 
     useEffect(() => {
+        if (isUsersSuccess)
+            setUsers(usersData?.data)
+    }, [users, isUsersSuccess, usersData])
+
+
+    useEffect(() => {
         if (isSuccess) {
             setChats(data.data)
             setPreFilteredChats(data.data)
@@ -40,12 +52,40 @@ function ChatsPage() {
     }, [isSuccess])
     return (
         <>
-           
-          
+            {error && error.response && error.response.data && error.response.data.message && <Typography color="red" p={2}>{error.response.data.message}</Typography>}
+            {isLoading && <LinearProgress />}
             < Stack direction="row" p={2} gap={2} alignItems={'center'} justifyContent={'space-between'}>
-                {error && error.response.data.message && <Typography color="red" p={2}></Typography>}
-                {isLoading && <LinearProgress />}
                 <Typography variant="button" fontWeight={'bold'}>WA Chats</Typography>
+                {user?.bot_access_fields.is_editable &&
+                    < TextField
+                        size='small'
+                        select
+                        SelectProps={{
+                            native: true,
+                        }}
+                        onChange={(e) => {
+                            setClientId(e.target.value)
+                            refetch()
+                        }}
+                        required
+                        id="todo_owner"
+                        label="Filter Todos Of Indivdual"
+                        fullWidth
+                    >
+                        <option key={'00'} value={undefined}>
+
+                        </option>
+                        {
+                            users.map((user, index) => {
+                                if (user.connected_number)
+                                    return (<option key={index} value={user.client_id}>
+                                        {user.username}
+                                    </option>)
+                                else
+                                    return null
+                            })
+                        }
+                    </TextField>}
                 <TextField
                     size="small"
                     onChange={(e) => setFilter(e.currentTarget.value)}
