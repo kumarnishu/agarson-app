860d98594235eaf30ada973acea9f82b26d35cfd
unknown <kumarnishu437@gmail.com>
2023-12-11 14:55:14 +0530

    updated

--- utils/HandleReminder.ts
+++ utils/HandleReminder.ts
@@ -65,16 +65,7 @@ export async function ReminderWithTemplates(reminder: IReminder, client: Client,
                     next_run_date: new Date(cron.sendAt(reminder.cron_string)),
                     is_paused: true
                 })
-                if (latest_reminder?.run_once) {
-                    await Reminder.findByIdAndUpdate(reminder._id, {
-                        next_run_date: null,
-                        is_active: false
-                    })
-                    if (ReminderManager.exists(reminder.refresh_key))
-                        ReminderManager.deleteJob(reminder.refresh_key)
-                    if (ReminderManager.exists(reminder.running_key))
-                        ReminderManager.deleteJob(reminder.running_key)
-                }
+
 
             })
         ReminderManager.add(reminder.refresh_key, reminder.refresh_cron_string, async () => {
@@ -98,7 +89,66 @@ export async function ReminderWithTemplates(reminder: IReminder, client: Client,
             ReminderManager.start(reminder.refresh_key)
             ReminderManager.start(reminder.running_key)
         }
+        // run once job
+        if (reminder?.run_once) {
+            let latest_reminder = await Reminder.findById(reminder._id).populate('templates')
+            if (latest_reminder && latest_reminder.is_active) {
+                let is_random = latest_reminder?.is_random_template
+                let templates = latest_reminder?.templates
+                let timegap = 10000
+                let timeinsec = 5000
+                let reports = await ContactReport.find({ reminder: latest_reminder, whatsapp_status: "pending" }).populate('contact')
+                for (let i = 0; i < reports.length; i++) {
+                    let report = reports[i]
+                    if (report?.whatsapp_status === "pending" && report.reminder_status === "pending") {
+                        const timeout = setTimeout(async () => {
+                            let sent = false
+                            console.log("running for messages", report.contact.mobile, new Date().toLocaleTimeString())
+                            let mobile = report.contact.mobile
+                            let name = report.contact.name
 
+                            let response = await sendTemplates(client, mobile, timegap, templates, is_random, reminder.serial_number, reminder.is_todo)
+                            if (response === "notwhatsapp") {
+                                report.whatsapp_status = "notwhatsapp"
+                                report.updated_at = new Date()
+                                await report.save()
+                                sent = false
+                            }
+                            if (response === "sent") {
+                                report.whatsapp_status = "sent"
+                                report.contact.name = name
+                                report.updated_at = new Date()
+                                await report.save()
+                                sent = true
+                            }
+                            if (response === "error") {
+                                report.whatsapp_status = "error"
+                                report.contact.name = name
+                                report.updated_at = new Date()
+                                await report.save()
+                                sent = false
+                            }
+                            if (sent && latest_reminder && !latest_reminder.is_todo) {
+                                latest_reminder.is_active = false
+                                await latest_reminder.save()
+                            }
+                        }, Number(timeinsec));
+                        reminder_timeouts.push({ id: reminder._id, timeout: timeout })
+                        timeinsec = timeinsec + Math.ceil(Math.random() * 3) * 1000
+                        console.log(timeinsec)
+                    }
+                }
+            }
+            await Reminder.findByIdAndUpdate(reminder._id, {
+                next_run_date: new Date(cron.sendAt(reminder.cron_string)),
+                is_active: false
+            })
+            if (latest_reminder && ReminderManager.exists(latest_reminder?.running_key))
+                ReminderManager.deleteJob(latest_reminder?.running_key)
+            if (latest_reminder && ReminderManager.exists(latest_reminder?.refresh_key))
+                ReminderManager.deleteJob(latest_reminder?.refresh_key)
+
+        }
 
     }
 }
@@ -161,16 +211,7 @@ export async function ReminderWithMessage(reminder: IReminder, client: Client, u
                     next_run_date: new Date(cron.sendAt(reminder.cron_string)),
                     is_paused: true
                 })
-                if (latest_reminder?.run_once) {
-                    await Reminder.findByIdAndUpdate(reminder._id, {
-                        next_run_date: null,
-                        is_active: false
-                    })
-                    if (ReminderManager.exists(reminder.refresh_key))
-                        ReminderManager.deleteJob(reminder.refresh_key)
-                    if (ReminderManager.exists(reminder.running_key))
-                        ReminderManager.deleteJob(reminder.running_key)
-                }
+
             })
 
         // refresh job
@@ -193,6 +234,63 @@ export async function ReminderWithMessage(reminder: IReminder, client: Client, u
         if (!reminder.run_once) {
             ReminderManager.start(reminder.refresh_key)
             ReminderManager.start(reminder.running_key)
+        }
+        if (reminder?.run_once) {
+            let latest_reminder = await Reminder.findById(reminder._id).populate('templates')
+            if (latest_reminder && latest_reminder.is_active) {
+                let message = latest_reminder?.message
+                let timegap = 10000
+                let timeinsec = 5000
+                let reports = await ContactReport.find({ reminder: latest_reminder, whatsapp_status: "pending" }).populate('contact')
+
+                for (let i = 0; i < reports.length; i++) {
+                    let report = reports[i]
+                    if (report?.whatsapp_status === "pending" && report.reminder_status === "pending") {
+                        const timeout = setTimeout(async () => {
+                            console.log("running for messages", report.contact.mobile, new Date().toLocaleTimeString())
+                            let mobile = report.contact.mobile
+                            let name = report.contact.name
+                            let sent = false
+                            let response = await sendMessage(client, mobile, timegap, message, reminder.serial_number, reminder.is_todo)
+                            if (response === "notwhatsapp") {
+                                report.whatsapp_status = "notwhatsapp"
+                                report.updated_at = new Date()
+                                await report.save()
+                                sent = false
+                            }
+                            if (response === "sent") {
+                                report.whatsapp_status = "sent"
+                                report.contact.name = name
+                                report.updated_at = new Date()
+                                await report.save()
+                                sent = true
+                            }
+                            if (response === "error") {
+                                report.whatsapp_status = "error"
+                                report.contact.name = name
+                                report.updated_at = new Date()
+                                await report.save()
+                                sent = false
+                            }
+                            if (sent && latest_reminder && !latest_reminder.is_todo) {
+                                latest_reminder.is_active = false
+                                await latest_reminder.save()
+                            }
+                        }, Number(timeinsec));
+                        reminder_timeouts.push({ id: reminder._id, timeout: timeout })
+                        timeinsec = timeinsec + Math.ceil(Math.random() * 3) * 1000
+                        console.log(timeinsec)
+                    }
+                }
+            }
+            await Reminder.findByIdAndUpdate(reminder._id, {
+                next_run_date: new Date(cron.sendAt(reminder.cron_string)),
+                is_active: false
+            })
+            if (latest_reminder && ReminderManager.exists(latest_reminder?.running_key))
+                ReminderManager.deleteJob(latest_reminder?.running_key)
+            if (latest_reminder && ReminderManager.exists(latest_reminder?.refresh_key))
+                ReminderManager.deleteJob(latest_reminder?.refresh_key)
         }
     }
 }
