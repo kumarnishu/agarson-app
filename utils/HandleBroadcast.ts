44d4f8f21a46a69e0269bae76b3834b337dd648e
nishu kumar <kumarnishu437@gmail.com>
2023-12-15 20:18:11 +0530

    updated

--- utils/HandleBroadcast.ts
+++ utils/HandleBroadcast.ts
@@ -14,66 +14,64 @@ export async function BroadCastWithTemplates(broadcast: IBroadcast, client: Clie
         let daily_limit = broadcast.daily_limit
         if (start_by_server && daily_limit > 0) {
             let latest_broadcast = await Broadcast.findById(broadcast._id).populate('templates')
-            if (latest_broadcast && latest_broadcast.is_active) {
-                if (!latest_broadcast.is_paused) {
-                    latest_broadcast.is_paused = false
-                    latest_broadcast.next_run_date = new Date(cron.sendAt(latest_broadcast.cron_string))
-                    await latest_broadcast.save()
-                    let is_random = latest_broadcast?.is_random_template
-                    let templates = latest_broadcast?.templates
-                    let timegap = Number(latest_broadcast?.time_gap) * 1000 || 10000
-                    let timeinsec = 5000
-                    let reports = await BroadcastReport.find({ broadcast: latest_broadcast, status: "pending" }).sort('-created_at').limit(daily_limit)
-                    if (reports.length > 0) {
-                        for (let i = 0; i < latest_broadcast.daily_limit - latest_broadcast.daily_count + 1; i++) {
-                            let report = reports[i]
-                            if (report?.status === "pending") {
-                                const timeout = setTimeout(async () => {
-                                    if (latest_broadcast) {
-                                        let mobile = report.mobile
-                                        let customer_name = report.customer_name
-                                        let is_buisness = report.is_buisness
-                                        let response = await sendTemplates(client, mobile, timegap, templates, is_random)
+            if (latest_broadcast && latest_broadcast.is_active && !latest_broadcast.is_paused) {
+                latest_broadcast.is_paused = false
+                latest_broadcast.next_run_date = new Date(cron.sendAt(latest_broadcast.cron_string))
+                await latest_broadcast.save()
+                let is_random = latest_broadcast?.is_random_template
+                let templates = latest_broadcast?.templates
+                let timegap = Number(latest_broadcast?.time_gap) * 1000 || 10000
+                let timeinsec = 5000
+                let reports = await BroadcastReport.find({ broadcast: latest_broadcast, status: "pending" }).sort('-created_at').limit(latest_broadcast.daily_limit - latest_broadcast.daily_count)
+                if (reports.length > 0) {
+                    for (let i = 0; i < reports.length; i++) {
+                        let report = reports[i]
+                        if (report?.status === "pending") {
+                            const timeout = setTimeout(async () => {
+                                if (latest_broadcast) {
+                                    let mobile = report.mobile
+                                    let customer_name = report.customer_name
+                                    let is_buisness = report.is_buisness
+                                    let response = await sendTemplates(client, mobile, timegap, templates, is_random)
 
-                                        latest_broadcast.daily_count = latest_broadcast.daily_count + 1
-                                        await latest_broadcast.save()
-                                        if (response === "notwhatsapp") {
-                                            report.status = "notwhatsapp"
-                                            report.updated_at = new Date()
-                                            await report.save()
-                                        }
-                                        if (response === "sent") {
-                                            report.status = "sent"
-                                            report.customer_name = customer_name
-                                            report.is_buisness = Boolean(is_buisness)
-                                            report.updated_at = new Date()
-
-                                            await report.save()
-                                        }
-                                        if (response === "error") {
-                                            report.status = "error"
-                                            report.customer_name = customer_name
-                                            report.is_buisness = Boolean(is_buisness)
-                                            report.updated_at = new Date()
-                                            await report.save()
-                                        }
+                                    latest_broadcast.daily_count = latest_broadcast.daily_count + 1
+                                    await latest_broadcast.save()
+                                    if (response === "notwhatsapp") {
+                                        report.status = "notwhatsapp"
+                                        report.updated_at = new Date()
+                                        await report.save()
                                     }
+                                    if (response === "sent") {
+                                        report.status = "sent"
+                                        report.customer_name = customer_name
+                                        report.is_buisness = Boolean(is_buisness)
+                                        report.updated_at = new Date()
 
-                                }, Number(timeinsec));
-                                timeouts.push({ id: broadcast._id, timeout: timeout })
-                                timeinsec = timeinsec + Number(broadcast.time_gap) * 1000 + Math.ceil(Math.random() * 3) * 1000
-                                console.log(timeinsec)
-                            }
+                                        await report.save()
+                                    }
+                                    if (response === "error") {
+                                        report.status = "error"
+                                        report.customer_name = customer_name
+                                        report.is_buisness = Boolean(is_buisness)
+                                        report.updated_at = new Date()
+                                        await report.save()
+                                    }
+                                }
+
+                            }, Number(timeinsec));
+                            timeouts.push({ id: broadcast._id, timeout: timeout })
+                            timeinsec = timeinsec + Number(broadcast.time_gap) * 1000 + Math.ceil(Math.random() * 3) * 1000
+                            console.log(timeinsec)
                         }
-                        const timeout = setTimeout(async () => {
-                            if (latest_broadcast) {
-                                latest_broadcast.is_paused = true
-                                latest_broadcast.daily_count = 0
-                                await latest_broadcast.save()
-                            }
-                        }, timeinsec)
-                        timeouts.push({ id: broadcast._id, timeout: timeout })
                     }
+                    const timeout = setTimeout(async () => {
+                        if (latest_broadcast) {
+                            latest_broadcast.is_paused = true
+                            latest_broadcast.daily_count = 0
+                            await latest_broadcast.save()
+                        }
+                    }, timeinsec)
+                    timeouts.push({ id: broadcast._id, timeout: timeout })
                 }
                 BroadCastWithTemplates(latest_broadcast, client, user)
             }
@@ -93,7 +91,7 @@ export async function BroadCastWithTemplates(broadcast: IBroadcast, client: Clie
                         let timeinsec = 5000
                         let reports = await BroadcastReport.find({ broadcast: latest_broadcast, status: "pending" }).sort('-created_at').limit(daily_limit)
                         if (reports.length === 0 && latest_broadcast.autoRefresh) {
-                            let dbreports = await BroadcastReport.find({ broadcast: latest_broadcast }).sort('-created_at')
+                            let dbreports = await BroadcastReport.find({ broadcast: latest_broadcast })
                             for (let i = 0; i < dbreports.length; i++) {
                                 let report = dbreports[i]
                                 if (user)
@@ -232,66 +230,64 @@ export async function BroadCastWithMessage(broadcast: IBroadcast, client: Client
         let daily_limit = Number(broadcast.daily_limit)
         if (start_by_server && daily_limit > 0) {
             let latest_broadcast = await Broadcast.findById(broadcast._id).populate('templates')
-            if (latest_broadcast && latest_broadcast.is_active) {
-                if (!latest_broadcast.is_paused) {
-                    latest_broadcast.is_paused = false
-                    latest_broadcast.next_run_date = new Date(cron.sendAt(latest_broadcast.cron_string))
-                    await latest_broadcast.save()
+            if (latest_broadcast && latest_broadcast.is_active && !latest_broadcast.is_paused) {
+                latest_broadcast.is_paused = false
+                latest_broadcast.next_run_date = new Date(cron.sendAt(latest_broadcast.cron_string))
+                await latest_broadcast.save()
 
-                    let timegap = Number(latest_broadcast?.time_gap) * 1000 || 10000
-                    let timeinsec = 5000
-                    let reports = await BroadcastReport.find({ broadcast: latest_broadcast, status: "pending" }).sort('-created_at').limit(daily_limit)
-                    if (reports.length > 0) {
-                        for (let i = 0; i < latest_broadcast.daily_limit - latest_broadcast.daily_count + 1; i++) {
-                            let report = reports[i]
-                            if (report?.status === "pending") {
-                                const timeout = setTimeout(async () => {
-                                    if (latest_broadcast) {
-                                        let mobile = report.mobile
-                                        let message = latest_broadcast.message
-                                        let customer_name = report.customer_name
-                                        let is_buisness = report.is_buisness
-                                        let response = await sendMessage(client, mobile, timegap, message)
+                let timegap = Number(latest_broadcast?.time_gap) * 1000 || 10000
+                let timeinsec = 5000
+                let reports = await BroadcastReport.find({ broadcast: latest_broadcast, status: "pending" }).sort('-created_at').limit(latest_broadcast.daily_limit - latest_broadcast.daily_count)
+                if (reports.length > 0) {
+                    for (let i = 0; i < reports.length; i++) {
+                        let report = reports[i]
+                        if (report?.status === "pending") {
+                            const timeout = setTimeout(async () => {
+                                if (latest_broadcast) {
+                                    let mobile = report.mobile
+                                    let message = latest_broadcast.message
+                                    let customer_name = report.customer_name
+                                    let is_buisness = report.is_buisness
+                                    let response = await sendMessage(client, mobile, timegap, message)
 
-                                        latest_broadcast.daily_count = latest_broadcast.daily_count + 1
-                                        await latest_broadcast.save()
-                                        if (response === "notwhatsapp") {
-                                            report.status = "notwhatsapp"
-                                            report.updated_at = new Date()
-                                            await report.save()
-                                        }
-                                        if (response === "sent") {
-                                            report.status = "sent"
-                                            report.customer_name = customer_name
-                                            report.is_buisness = Boolean(is_buisness)
-                                            report.updated_at = new Date()
-
-                                            await report.save()
-                                        }
-                                        if (response === "error") {
-                                            report.status = "error"
-                                            report.customer_name = customer_name
-                                            report.is_buisness = Boolean(is_buisness)
-                                            report.updated_at = new Date()
-                                            await report.save()
-                                        }
+                                    latest_broadcast.daily_count = latest_broadcast.daily_count + 1
+                                    await latest_broadcast.save()
+                                    if (response === "notwhatsapp") {
+                                        report.status = "notwhatsapp"
+                                        report.updated_at = new Date()
+                                        await report.save()
                                     }
+                                    if (response === "sent") {
+                                        report.status = "sent"
+                                        report.customer_name = customer_name
+                                        report.is_buisness = Boolean(is_buisness)
+                                        report.updated_at = new Date()
 
-                                }, Number(timeinsec));
-                                timeouts.push({ id: broadcast._id, timeout: timeout })
-                                timeinsec = timeinsec + Number(broadcast.time_gap) * 1000 + Math.ceil(Math.random() * 3) * 1000
-                                console.log(timeinsec)
-                            }
+                                        await report.save()
+                                    }
+                                    if (response === "error") {
+                                        report.status = "error"
+                                        report.customer_name = customer_name
+                                        report.is_buisness = Boolean(is_buisness)
+                                        report.updated_at = new Date()
+                                        await report.save()
+                                    }
+                                }
+
+                            }, Number(timeinsec));
+                            timeouts.push({ id: broadcast._id, timeout: timeout })
+                            timeinsec = timeinsec + Number(broadcast.time_gap) * 1000 + Math.ceil(Math.random() * 3) * 1000
+                            console.log(timeinsec)
                         }
-                        const timeout = setTimeout(async () => {
-                            if (latest_broadcast) {
-                                latest_broadcast.is_paused = true
-                                latest_broadcast.daily_count = 0
-                                await latest_broadcast.save()
-                            }
-                        }, timeinsec)
-                        timeouts.push({ id: broadcast._id, timeout: timeout })
                     }
+                    const timeout = setTimeout(async () => {
+                        if (latest_broadcast) {
+                            latest_broadcast.is_paused = true
+                            latest_broadcast.daily_count = 0
+                            await latest_broadcast.save()
+                        }
+                    }, timeinsec)
+                    timeouts.push({ id: broadcast._id, timeout: timeout })
                 }
                 BroadCastWithMessage(latest_broadcast, client, user)
             }
