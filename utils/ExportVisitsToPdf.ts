5ba34cec6d70c402e7b5c89c0a785cdc46d42cd6
unknown <kumarnishu437@gmail.com>
2024-01-17 14:23:51 +0530

    updated

--- utils/ExportVisitsToPdf.ts
+++ utils/ExportVisitsToPdf.ts
@@ -10,9 +10,8 @@ import { ReportManager } from "../app"
 
 
 export async function handleVisitReport(client: { client_id: string, client: any }) {
-    // let cronString1 = `20 18 1/1 * *`
-    let cronString1 = `16 14 1/1 * *`
-    let cronString2 = `20 14 1/1 * *`
+    let cronString1 = `20 18 1/1 * *`
+    let cronString2 = `28 14 1/1 * *`
     console.log("running trigger")
     if (!ReportManager.exists("visit_reports1"))
         ReportManager.add("visit_reports1", cronString1, async () => {
